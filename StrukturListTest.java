c6f98e81a1bef3b691b4cbc63fc977ed0129d5c4
Ginamei <116251200+Ginamei@users.noreply.github.com>
2024-03-17 14:05:59 +0700

    Add files via upload

---
+++ StrukturListTest.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas6;
+
+/**
+ *
+ * @author Gina Meirina
+ */
+public class StrukturListTest {
+     public static void main(String[] args) {
+        // 1. Create list dengan keyword new
+        StrukturList myList = new StrukturList();
+        
+        // 2. Tambah elemen 5 di akhir list
+        myList.addTail(5);
+        
+        // 3. Tambah elemen 4 di akhir list
+        myList.addTail(4);
+        
+        // 4. Tambah elemen 6 di akhir list
+        myList.addTail(6);
+        
+        // 5. Tampilkan elemen list
+        System.out.print("List elements: ");
+        myList.displayElement();
+        System.out.println(); // New line for clarity
+        
+        // 6. Cari elemen bernilai 6
+        int searchElement = 6;
+        System.out.println(myList.find(searchElement));
+    }
+}
